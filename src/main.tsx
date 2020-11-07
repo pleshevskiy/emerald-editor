1396f7ee57b2acbb4f8b04d9a834906b20455f06
Dmitriy Pleshevskiy <dmitriy@ideascup.me>
2020-11-08 02:12:45 +0300

    chore: change layout

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,8 @@ export function EmeraldEditor() {
     return (
         <DndProvider backend={HTML5Backend}>
             <div className="row-start-stretch grow-1">
-                <div className="col-start-start grow-1 p-16 bg-for-test">1</div>
-                <div className="col-start-start grow-3 p-16 bg-for-test">2</div>
-                <div className="col-start-start grow-1 p-16 bg-for-test">3</div>
+                <div className="col-start-start grow-1 p-16 bg-for-test">2</div>
+                <div className="col-start-start w-320 p-16 bg-for-test">3</div>
             </div>
         </DndProvider>
     );
