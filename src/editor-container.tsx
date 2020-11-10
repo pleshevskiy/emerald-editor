7380604a85f82b4a5b8c8872ecf5fdf4eb5140dc
Dmitriy Pleshevskiy <dmitriy@ideascup.me>
2020-11-11 00:01:50 +0300

    feat: add separator for drop components
    feat(component): use separator index for dropping
    feat(component): add possibility to move
    feat(component): show new place before drop
    fix: change trash z-index
    
    Closes #5

--- src/editor-container.tsx
+++ src/editor-container.tsx
@@ -1,58 +1,76 @@
 import React from 'react';
-import { useDrop } from 'react-dnd';
-import { Component, ComponentType } from './component';
+import { Component } from './component';
+import { DropComponentSeparator } from './drop-component-separator';
 import { DragItem } from './interfaces';
 import { Trash } from './trash';
 
 
+function shiftComponentIndex(diff = 1) {
+    return function (component: Required<DragItem>) {
+        return {
+            ...component,
+            index: component.index + diff,
+        };
+    };
+}
+
 export function EditorContainer() {
     const [components, setComponents] = React.useState<Required<DragItem>[]>([]);
-
-    const [{ didDrop }, drop] = useDrop({
-        accept: [
-            ComponentType.Layout,
-        ],
-        drop(item: DragItem) {
-            if (!didDrop) {
-                setComponents([
-                    ...components,
-                    {
-                        ...item,
-                        index: components.length,
-                    }
-                ]);
-            }
-        },
-        collect: (monitor) => ({
-            didDrop: monitor.didDrop(),
-        }),
-    });
 
     const onDropToTrash = React.useCallback(
         (item) => {
             if (item.index != null) {
                 setComponents([
                     ...components.slice(0, item.index),
-                    ...components.slice(item.index + 1).map(item => ({
-                        ...item,
-                        index: item.index - 1,
-                    })),
+                    ...components.slice(item.index + 1).map(shiftComponentIndex()),
                 ]);
             }
         },
         [components]
     );
 
+    const onDropComponent = React.useCallback(
+        (item: DragItem, index: number) => {
+            const componentsWithoutDraggedItem = item.index == null ?
+                components
+                : [
+                    ...components.slice(0, item.index),
+                    ...components.slice(item.index + 1)
+                ];
+
+            const newComponents = [
+                ...componentsWithoutDraggedItem.slice(0, index),
+                {
+                    ...item,
+                    id: item.id ?? Math.ceil(Math.random() * 100000),
+                    index,
+                },
+                ...componentsWithoutDraggedItem.slice(index)
+            ].map((item, index) => ({ ...item, index }));
+
+            setComponents(newComponents);
+        },
+        [components]
+    );
+
     return (
         <div
-            ref={drop}
             className="relative col-start-center grow-1 w-100p min-h-100 bg-white"
         >
             <Trash onDrop={onDropToTrash} />
-            <p>Drop here</p>
+            <DropComponentSeparator
+                index={0}
+                onDrop={onDropComponent}
+            />
 
             {components.map((item, i) => (
-                <Component key={i} type={item.type} item={item} />
+                <React.Fragment key={i}>
+                    <Component type={item.type} item={item} />
+                    <DropComponentSeparator
+                        index={i + 1}
+                        onDrop={onDropComponent}
+                    />
+                </React.Fragment>
             ))}
         </div>
     );
